246da89f7790733cb0f7e1700a6e5c4b00863d12
Catalin Murariu <catalinmurariu@yahoo.com>
2014-04-13 12:41:16 +0100

    - sass setup

--- ReadPlayCode/App_Start/BundleConfig.cs
+++ ReadPlayCode/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace ReadPlayCode.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/base-theme/basestyle.css"));
         }
     }
 }
